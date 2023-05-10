342a46f5229f551b06073e4ffac0fc83a40dc0a4
Sunny kumar <120244832+sunny8750@users.noreply.github.com>
2023-05-10 13:46:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // your code here
+  function updateURL() {
+      var name = document.getElementById("name").value;
+      var year = document.getElementById("year").value;
+      var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + year;
+      document.getElementById("url").textContent = url;
+    }
